dad06bdcf2e558417f15481fbea2f03d39af3131
Fawad Halim <fawad@fawad.net>
2014-07-23 16:08:09 -0500

    Added hit counter. Make hazelcast only use localhost.

--- src/main/scala/net/fawad/learningscala/JavaMain.java
+++ src/main/scala/net/fawad/learningscala/JavaMain.java
@@ -2,6 +2,7 @@ package net.fawad.learningscala;
 
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IAtomicLong;
 import com.hazelcast.core.ILock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -16,7 +17,9 @@ public class JavaMain {
             final ILock lock = h.getLock("springapp");
             try{
                 lock.lock();
-                try(ConfigurableApplicationContext ignored = SpringApplication.run(Controllers.class, args)){
+                try(ConfigurableApplicationContext ctx = SpringApplication.run(Controllers.class, args)){
+                    final IAtomicLong hitCount = h.getAtomicLong("hitcounter");
+                    ctx.getBeanFactory().registerSingleton("hitCount", hitCount);
                     new CountDownLatch(1).await();
                 }
             }finally{
--- src/main/scala/net/fawad/learningscala/Main.scala
+++ src/main/scala/net/fawad/learningscala/Main.scala
@@ -2,20 +2,28 @@ package net.fawad.learningscala
 
 import java.util.concurrent.CountDownLatch
 
-import com.hazelcast.core.Hazelcast
+import com.hazelcast.config.Config
+import com.hazelcast.core.{Hazelcast, IAtomicLong}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody}
 import resource.managed
 
+import scala.annotation.meta.setter
+
 object Main {
   def main(args: Array[String]) {
     import net.fawad.learningscala.Disposal._
-
+    val cfg = new Config
     for (h <- managed(Hazelcast.newHazelcastInstance());
          lock <- managed(h.getLock("springapp"));
          ctx <- managed(SpringApplication.run(classOf[Controllers], args: _*))) {
+      val hitCount = h.getAtomicLong("hitcounter")
+      ctx.getBeanFactory.registerSingleton("hitCount", hitCount)
       new CountDownLatch(1).await()
     }
   }
@@ -23,8 +31,11 @@ object Main {
 
 @Controller
 @EnableAutoConfiguration
-@RequestMapping(Array("/api")) class Controllers {
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@RequestMapping(Array("/api"))
+class Controllers {
+  @(Autowired @setter)
+  var hitCounter:IAtomicLong = _
   @RequestMapping(Array("/hello"))
-  @ResponseBody def sayHello = "Hello world"
+  @ResponseBody def sayHello = "Hello world " + hitCounter.addAndGet(1)
 }
-
